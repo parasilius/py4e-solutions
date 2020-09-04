050cac7701d52a2a7aa7328e00497dc98d5798a2
DrNakiz <parsahosseini2001@gmail.com>
2020-09-04 18:57:01 +0430

    Added Chapter 12

---
+++ urllinks.py
@@ -0,0 +1,23 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+count = 0
+for tag in tags:
+    count += 1
+print(count)
